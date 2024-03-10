a6c5e6ba27c3da271a60d236a6b6305c4090bd07
camilin69 <camilo.merchan02@uptc.edu.co>
2024-03-10 03:44:07 -0500

    Dejé hecho la parte de mensajes y guardados

--- funciones/cuenta.js
+++ funciones/cuenta.js
@@ -7,3 +7,14 @@ function noSubrayado(){
     var boton = document.getElementById("crearCuenta");
     boton.style.setProperty("text-decoration","unset")
 }
+
+function registrarUsuario(){
+    var primerNombre = document.getElementsByClassName("primerNombre")[0];
+    var segundoNombre = document.getElementsByClassName("segundoNombre")[0];
+    var identificacion = document.getElementsByClassName("identificacion")[0];
+    var correo = document.getElementsByClassName("correo")[0];
+    var contraseña = document.getElementsByClassName("contraseña")[0];
+
+    
+
+}
